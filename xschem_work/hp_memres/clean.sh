6e9ef49ff0b2491a4f0c91066df12ad194ed9707
Rodrigo <rodrigoalvarez449@gmail.com>
2024-06-14 14:42:48 -0600

    Add V1 for HP memristor with Joglekar function

---
+++ xschem_work/hp_memres/clean.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+
+rm /usr/local/share/pdk/sky130B/libs.tech/ngspice/mem_hp.va
+rm /usr/local/share/pdk/sky130B/libs.tech/ngspice/mem_hp.spice
+rm /usr/local/share/pdk/sky130B/libs.tech/ngspice/mem_hp.osdi
+rm /usr/local/share/pdk/sky130B/libs.tech/xschem/sky130_fd_pr/mem_hp.sym
---
+++ xschem_work/hp_memres/install.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+
+cp mem_hp.va /usr/local/share/pdk/sky130B/libs.tech/ngspice
+cp mem_hp.spice /usr/local/share/pdk/sky130B/libs.tech/ngspice
+cp mem_hp.osdi /usr/local/share/pdk/sky130B/libs.tech/ngspice
+cp mem_hp.sym /usr/local/share/pdk/sky130B/libs.tech/xschem/sky130_fd_pr
